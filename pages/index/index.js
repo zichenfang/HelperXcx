1a6e7e5ca9c099074bf1ee6931bef0dddde400f9
zichenfang <zichenfang@yeah.net>
2018-10-30 00:06:55 +0800

    picker

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,16 @@ const app = getApp()
 
 Page({
   data: {
-
+    array: ['美国', '中国', '巴西', '日本'],
   },
   onLoad: function () {
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value);
+    console.log(e);
+    this.setData({
+      index: e.detail.value
+    })
   },
 
 })
