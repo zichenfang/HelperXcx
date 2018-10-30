5101885c40f2178e015c3568bc915aa32e6f3abc
fangzichen <zichenfang@yeah.net>
2018-10-30 18:57:33 +0800

    no message

--- pages/index/docList.js
+++ pages/index/docList.js
@@ -1,4 +1,4 @@
-// pages/PDocsList.js
+// pages/index/docList.js
 Page({
 
   /**
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options);
   },
 
   /**
